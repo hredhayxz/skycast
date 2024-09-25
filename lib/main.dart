79176e4fb72843f8818a7b360266eb9a4288a6d5
Md Alhaz Mondal Hredhay <amhredhay102@gmail.com>
2024-09-26 00:35:40 +0600

    Home screen initial design has been done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ import 'package:skycast/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light,
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent),
+  );
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
     (_) {
       runApp(const SkyCastApp());
--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:skycast/views/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await Future.delayed(const Duration(seconds: 3)).then((_) {
+        if (mounted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()),
+          );
+        }
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
